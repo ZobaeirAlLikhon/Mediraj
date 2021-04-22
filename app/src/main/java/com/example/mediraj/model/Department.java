d13003b25a96853b882eb142f1c011006cae5a7f
ZobaeirAlLikhon <ZobaeirAlLikhon@gmail.com>
2021-04-22 14:07:38 +0600

    fix some error

---
+++ app/src/main/java/com/example/mediraj/model/Department.java
@@ -0,0 +1,135 @@
+package com.example.mediraj.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Department {
+    @SerializedName("message")
+    @Expose
+    private String message;
+    @SerializedName("response")
+    @Expose
+    private String response;
+    @SerializedName("status")
+    @Expose
+    private String status;
+    @SerializedName("data")
+    @Expose
+    private List<Datum> data = null;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<Datum> getData() {
+        return data;
+    }
+
+    public void setData(List<Datum> data) {
+        this.data = data;
+    }
+
+    public class Datum {
+
+        @SerializedName("id")
+        @Expose
+        private Integer id;
+        @SerializedName("category_id")
+        @Expose
+        private Integer categoryId;
+        @SerializedName("title")
+        @Expose
+        private String title;
+        @SerializedName("status")
+        @Expose
+        private String status;
+        @SerializedName("created_at")
+        @Expose
+        private String createdAt;
+        @SerializedName("updated_at")
+        @Expose
+        private String updatedAt;
+        @SerializedName("category_title")
+        @Expose
+        private String categoryTitle;
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public Integer getCategoryId() {
+            return categoryId;
+        }
+
+        public void setCategoryId(Integer categoryId) {
+            this.categoryId = categoryId;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public String getCreatedAt() {
+            return createdAt;
+        }
+
+        public void setCreatedAt(String createdAt) {
+            this.createdAt = createdAt;
+        }
+
+        public String getUpdatedAt() {
+            return updatedAt;
+        }
+
+        public void setUpdatedAt(String updatedAt) {
+            this.updatedAt = updatedAt;
+        }
+
+        public String getCategoryTitle() {
+            return categoryTitle;
+        }
+
+        public void setCategoryTitle(String categoryTitle) {
+            this.categoryTitle = categoryTitle;
+        }
+
+    }
+}
