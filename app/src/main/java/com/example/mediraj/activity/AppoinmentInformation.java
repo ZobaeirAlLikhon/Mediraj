f95ff0992cfe57f0d8bdb103239c36b46a220d2f
akash-svg <akash001karim@gmail.com>
2021-05-29 01:34:37 +0600

    Diagonistic cardView ok

---
+++ app/src/main/java/com/example/mediraj/activity/AppoinmentInformation.java
@@ -0,0 +1,16 @@
+package com.example.mediraj.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mediraj.R;
+
+public class AppoinmentInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appoinment_information);
+    }
+}
